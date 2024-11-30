0dbf4dd0b2ba3bf76f949943f13d21b71aece8ef
Matthew <matthewmintz0818@gmail.com>
2024-11-30 15:06:01 -0500

    Connected to mongodb

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log('mongo-db connected'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('welcome to pharma-see API');
+});
+
+app.listen(PORT, () => {
+    console.log('server running on port ${PORT}');
+})
